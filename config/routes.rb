c970cf72a78b6935dd4b175c00d19b14bc532968
swapno <swapno7064@gmail.com>
2021-07-14 01:08:47 +0530

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  resources :users, expect: [:new]
 end
